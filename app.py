62656badfd6fa032e5b9d10b7f6876bdcbcf43ee
jsaha26 <jyotiraditya.saha26@gmail.com>
2024-03-30 01:10:25 +0530

    add API, styling

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 import config
-import routes
 import models
+import api
+import routes
 
 if __name__ == '__main__':
     app.run(debug=True)
