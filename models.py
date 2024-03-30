f38eeeda285c6b9789b39f61524f832b3763dd4f
jsaha26 <jyotiraditya.saha26@gmail.com>
2024-03-30 15:27:20 +0530

    Fix the Checkout functionality, add print and export for user

--- models.py
+++ models.py
@@ -1,61 +1,58 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from werkzeug.security import generate_password_hash
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(32), unique=True, nullable = False)
-    passhash = db.Column(db.String(256), nullable = False)
-    name = db.Column(db.String(64), nullable = True)
-    is_admin = db.Column(db.Boolean, nullable = False, default = False)
+    username = db.Column(db.String(32), unique=True)
+    passhash = db.Column(db.String(256), nullable=False)
+    name = db.Column(db.String(64), nullable=True)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable = False)
+    name = db.Column(db.String(32), unique=True)
 
-    ## relationships
     products = db.relationship('Product', backref='category', lazy=True, cascade='all, delete-orphan')
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), nullable = False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable = False)
-    quantity = db.Column(db.Integer, nullable = False)
-    price = db.Column(db.Float, nullable = False)
-    man_date = db.Column(db.Date, nullable = False)
-    
-    ## relationships
+    name = db.Column(db.String(64), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    man_date = db.Column(db.Date, nullable=False)
+
     carts = db.relationship('Cart', backref='product', lazy=True, cascade='all, delete-orphan')
     orders = db.relationship('Order', backref='product', lazy=True, cascade='all, delete-orphan')
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable = False)
-    quantity = db.Column(db.Integer, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
 
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    total = db.Column(db.Float, nullable = False)
-    
-    ## relationships
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    datetime = db.Column(db.DateTime, nullable=False)
+    total = db.Column(db.Float, nullable=False, default=0.0)  # Assuming total is a float value
+
     orders = db.relationship('Order', backref='transaction', lazy=True, cascade='all, delete-orphan')
+
+
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable = False)
-    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable = False)
-    quantity = db.Column(db.Integer, nullable = False)
-    price = db.Column(db.Float, nullable = False)
+    transaction_id = db.Column(db.Integer, db.ForeignKey('transaction.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
 
-# create database if it doesn't exist
+
 with app.app_context():
     db.create_all()
-
     # if admin exists, else create admin
     admin = User.query.filter_by(is_admin=True).first()
     if not admin:
